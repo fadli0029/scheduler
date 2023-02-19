c490ecedb586530d9aa30989d012e0bd83faadba
fadli0029 <mfarsani@ucsd.edu>
2023-02-19 13:22:18 -0800

    [ADD] form with pysript

---
+++ docs/form.md
@@ -0,0 +1,11 @@
+# Form Page
+
+<form action="submit" method="post">
+  <label for="name">Name:</label>
+  <input type="text" id="name" name="name"><br>
+
+  <label for="age">Age:</label>
+  <input type="text" id="age" name="age"><br>
+
+  <input type="submit" value="Submit">
+</form>
---
+++ docs/form.py
@@ -0,0 +1,21 @@
+import os
+import sys
+
+form_data = sys.stdin.read()
+
+# Extract the name and age from the form data
+name = ""
+age = ""
+for line in form_data.split("\n"):
+    if "name=" in line:
+        name = line.split("=")[1]
+    if "age=" in line:
+        age = line.split("=")[1]
+
+# Write the form data to a table in a new markdown file
+with open("docs/table.md", "a") as f:
+    f.write(f"| {name} | {age} |\n")
+
+# Redirect the user back to the form page
+print("Status: 303 See Other")
+print(f"Location: {os.environ['HTTP_REFERER']}")
